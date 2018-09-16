23ed7e20cda13c8d12ba3f2d2808560502e15c8b
Vladimir Melnick <egotraumatic@gmail.com>
2018-09-16 18:41:25 +0300

    initial commit

---
+++ stdOUT.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	myString := ""
+	arguments := os.Args
+	if len(arguments) == 1 {
+		myString = "Please give my one argument!"
+	} else {
+		myString = arguments[1]
+	}
+	io.WriteString(os.Stdout, myString)
+	io.WriteString(os.Stdout, "\n")
+}
