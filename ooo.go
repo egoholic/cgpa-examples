6bca0ea25039cbbe84c9fc966f54d6f44c657763
Vladimir Melnick <egotraumatic@gmail.com>
2018-10-10 08:52:04 +0300

    adds ooo.go

---
+++ ooo.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type a struct {
+	XX, YY int
+}
+
+type b struct {
+	AA string
+	XX int
+}
+
+type c struct {
+	A a
+	B b
+}
+
+func (A a) A() {
+	fmt.Println("Function A() for A")
+}
+
+func (B b) A() {
+	fmt.Println("Function A() from B")
+}
+
+func main() {
+	var i c
+	i.A.A()
+	i.B.A()
+}
