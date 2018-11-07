d6666f510093066d6546b2cd4282b891970448bf
Volodymyr Melnyk <vmelnyk@tradetracker.com>
2018-11-07 12:19:29 +0200

    adds testWWW.go

---
+++ testWWW_test.go
@@ -0,0 +1,47 @@
+// $ go test testWWW.go testWWW_test.go -v
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCheckStatusOK(t *testing.T) {
+	req, err := http.NewRequest("GET", "/CheckStatusOK", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(CheckStatusOK)
+	handler.ServeHTTP(rr, req)
+
+	status := rr.Code
+	if status != http.StatusOK {
+		t.Errorf("handler returner %v", status)
+	}
+	expect := `Fine!`
+	if rr.Body.String() != expect {
+		t.Errorf("handler returned %v", rr.Body.String())
+	}
+}
+
+func TestStatusNotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/StatusNotFound", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(StatusNotFound)
+	handler.ServeHTTP(rr, req)
+
+	status := rr.Code
+	if status != http.StatusNotFound {
+		t.Errorf("handler returned %v", status)
+	}
+}
